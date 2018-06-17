4d364bfaa51b7a3a454fd14d49561bbfa160ffaa
Moyeong Lee <2000prcs@gmail.com>
2018-06-17 15:00:58 -0700

    Complete StringRotation

--- Arrays&Strings/StringRotation.js
+++ Arrays&Strings/StringRotation.js
@@ -3,26 +3,38 @@
 // one call to isSubstring (e.g.,"waterbottle" is a rotation of "erbottlewat").
 
 const rotateString = (str1, str2) => {
-  let firstLetter = str1[0];
-  let leftSide;
-  let rightSide;
-  let splited = str2.split('');
-  let newString = '';
-  
-  for(let i = 0; i < splited.length; i++){
-    if(splited[i] === firstLetter){
-      leftSide = splited.slice(0, i);
-      rightSide = splited.slice(i);    
-    }
-  }
-  
-  newString += rightSide.join('') + leftSide.join('');
-
-  if(newString === str1){
-    return true;
-  } else {
+  if(str1.length !== str2.length){
     return false;
+  } else {
+    return isSubstring(str1+str1, str2);
   }
+
+  function isSubstring(doubleStr1, str2){
+    return doubleStr1.includes(str2);
+  }
+
+// Solution2: slice and join
+
+  // let firstLetter = str1[0];
+  // let leftSide;
+  // let rightSide;
+  // let splited = str2.split('');
+  // let newString = '';
+  
+  // for(let i = 0; i < splited.length; i++){
+  //   if(splited[i] === firstLetter){
+  //     leftSide = splited.slice(0, i);
+  //     rightSide = splited.slice(i);    
+  //   }
+  // }
+  
+  // newString += rightSide.join('') + leftSide.join('');
+
+  // if(newString === str1){
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 
@@ -30,3 +42,7 @@ const rotateString = (str1, str2) => {
 
 var result = rotateString('waterbottle', 'erbottlewat');
 console.log(result);
+var result2 = rotateString('moeric', 'ericmo');
+console.log(result2);
+var result3 = rotateString('momeri', 'ericmo');
+console.log(result3);
