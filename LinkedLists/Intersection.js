b51f7a4c1b10577e96c6bd521e9f304a6b97d28a
Moyeong Lee <2000prcs@gmail.com>
2018-06-22 17:25:11 -0700

    Complete LoopDetection

--- LinkedLists/Intersection.js
+++ LinkedLists/Intersection.js
@@ -1,14 +1,50 @@
-const { LinkedList } = require('../Data Structures/LinkedList');
+const { LinkedList, Node } = require('../Data Structures/LinkedList');
 
 // Given two (singly) linked lists, determine if the two lists intersect. 
 // Return the inter­ secting node. Note that the intersection is defined based on reference, not value. 
 // That is, if the kth node of the first linked list is the exact same node (by reference) 
 // as the jth node of the second linked list, then they are intersecting.
 
-const findIntersection = () => {
-
-
-
+const findIntersection = (head1, head2) => {
+  let oldHead = head2;
+  while(head1){
+    while(head2){
+      if(head1.value === head2.value){
+        if(head1 === head2){
+          return head1.value;
+        }
+      }
+      head2 = head2.next;
+    }
+    head2 = oldHead;
+    head1 = head1.next;
+  }
+  return undefined;
 }
 
 
+let node1 = new Node(3);
+let node2 = new Node(1);
+let node3 = new Node(5);
+let node4 = new Node(9);
+let node5 = new Node(7);
+let node6 = new Node(2);
+let node7 = new Node(1);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node6;
+node6.next = node7;
+
+let node8 = new Node(4);
+let node9 = new Node(6);
+
+node8.next = node9;
+node9.next = node5;
+
+console.log(node1, node8);
+
+let result = findIntersection(node1, node8);
+console.log(result);
--- LinkedLists/LoopDetection.js
+++ LinkedLists/LoopDetection.js
@@ -1,3 +1,5 @@
+const { LinkedList, Node } = require('../Data Structures/LinkedList');
+
 // Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
 // DEFINITION
 // Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, 
@@ -5,3 +7,32 @@
 // EXAMPLE
 // Input: A -> B -> C -> D -> E -> C [the same C as earlier]
 // Output: C
+
+const detectLoop = (head) => {
+  let nodes = {};
+  while(head){
+    if(nodes[head.value]){
+      if(head === nodes[head.value]){
+        return head.value;
+      }
+    } 
+    nodes[head.value] = head;
+    head = head.next;
+  }
+  console.log(nodes)
+}
+
+let nodeA = new Node('A');
+let nodeB = new Node('B');
+let nodeC = new Node('C');
+let nodeD = new Node('D');
+let nodeE = new Node('E');
+
+nodeA.next = nodeB;
+nodeB.next = nodeC;
+nodeC.next = nodeD;
+nodeD.next = nodeE;
+nodeE.next = nodeC;
+
+let result = detectLoop(nodeA);
+console.log(result);
