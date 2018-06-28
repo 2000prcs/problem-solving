8428255465ae16d585f40aebd2bf19d40a8b45e3
Moyeong Lee <2000prcs@gmail.com>
2018-06-27 23:39:30 -0700

    Refactor MinimalTree

--- Trees & Graphs/ListOfDepths.js
+++ Trees & Graphs/ListOfDepths.js
@@ -1,5 +1,5 @@
-const { BinarySearchTree } = require('../DataStructures/BinarySearchTree.js');
-const { LinkedList } = require('../DataStructures/LinkedList.js');
+const { BinarySearchTree } = require('../Data Structures/BinarySearchTree');
+const { LinkedList } = require('../Data Structures/LinkedList');
 
 //Given a binary tree, design an algorithm which creates a linked list of all the nodes
 // at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
--- Trees & Graphs/MinimalTree.js
+++ Trees & Graphs/MinimalTree.js
@@ -1,4 +1,4 @@
-const { BinarySearchTree } = require('../DataStructures/BinarySearchTree');
+const { BinarySearchTree } = require('../Data Structures/BinarySearchTree');
 
 //Given a sorted (increasing order) array with unique integer elements, 
 //write an algo­rithm to create a binary search tree with minimal height.
