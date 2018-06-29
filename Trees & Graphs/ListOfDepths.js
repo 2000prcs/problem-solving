3860e784514d73f39c2554d98829858361f16336
Moyeong Lee <2000prcs@gmail.com>
2018-06-28 22:43:38 -0700

    Refactor ListofDepths

--- Trees & Graphs/ListOfDepths.js
+++ Trees & Graphs/ListOfDepths.js
@@ -4,33 +4,60 @@ const { LinkedList } = require('../Data Structures/LinkedList');
 //Given a binary tree, design an algorithm which creates a linked list of all the nodes
 // at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
+// Solution 1: Recursive approach
+// Time: O(N)
+// Additional space: 0(log N) space
 
+// const listOfDepths = (bst) => {
+//   let lists = [];
+//   addToList(lists, bst, 0);
+//   return lists;
+// }
+
+// const addToList = (lists, node, depth) => {
+//   if(node){
+//     if(!lists[depth]){
+//       lists[depth] = new LinkedList();
+//     }
+//     lists[depth].addToTail(node.value);
+//     addToList(lists, node.left, depth + 1);
+//     addToList(lists, node.right, depth + 1);
+//   }
+// }
+
+
+// Solution2: Iterable approach
 
 const listOfDepths = (bst) => {
   let listsOfLists = [];
   let list = null;
   let queue = []; 
-  let newQueue = [];
   let newNode;
   let currentNode = bst;
+  let childrenCount = 1;
+  let count = 0;
   queue.push(currentNode);
   while(queue.length > 0){
     currentNode = queue.shift();
-    newNode = new LinkedList(currentNode.value);
-    newNode.next = list;
-    list = newNode;
-    if(currentNode.left){
-      queue.push(currentNode.left);
+    if(list){
+      list.addToTail(currentNode.value);  
+    } else if (!list){
+        newNode = new LinkedList();
+        newNode.addToTail(currentNode.value);
+        list = newNode;  
     }
-    if(currentNode.right){
-      queue.push(currentNode.right);
-    }
-    if(queue.length === 0){
+    count++;
+    if(count === childrenCount){
       listsOfLists.push(list);
       list = null;
-      queue = newQueue;
-      newQueue = [];
-    }
+      childrenCount = childrenCount * 2 + 1;
+    }    
+     if(currentNode.left){
+       queue.push(currentNode.left);
+      }
+     if(currentNode.right){
+       queue.push(currentNode.right);
+     }
 
   }
   return listsOfLists;
@@ -44,4 +71,5 @@ tree.insert(1);
 tree.insert(3);
 tree.insert(5);
 tree.insert(7);
+console.log(tree);
 console.log(listOfDepths(tree));
