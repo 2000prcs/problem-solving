3c4b1c63da476df20d8fc8e72f9607e66d1af589
Moyeong Lee <2000prcs@gmail.com>
2018-07-01 14:28:09 -0700

    Complete MinimumBribes

--- Hacker Rank/MinimumBribes.js
+++ Hacker Rank/MinimumBribes.js
@@ -10,5 +10,26 @@
 // output: Too chaotic
 
 function minimumBribes(q) {
-
+  let bribes = 0;
+  for(let i = q.length - 1; i >= 0; i--){
+    if(q[i] - (i + 1) > 2){
+      console.log('Too chaotic');
+      return;
+    }
+    for(let j = Math.max(0, q[i] - 2); j < i; j++){
+      if(q[j] > q[i]) bribes++;
+    }
+  }
+  console.log(bribes);
+  return bribes;
 }
+
+
+let bribes = minimumBribes([2,1,5,3,4]);
+let bribes2 = minimumBribes([2,5,1,3,4]);
+let bribes3 = minimumBribes([ 5, 1, 2, 3, 7, 8, 6, 4 ]);
+let bribes4 = minimumBribes([ 1, 2, 5, 3, 7, 8, 6, 4 ]);
+console.log(bribes);
+console.log(bribes2);
+console.log(bribes3);
+console.log(bribes4);
