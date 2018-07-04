7d33d00f21c7e325b068e01ce0cad06720dfe2f8
Moyeong Lee <2000prcs@gmail.com>
2018-07-04 15:12:29 -0700

    Complete BSTSequences & ValidSudoku

---
+++ Leetcode/ValidSudoku.js
@@ -0,0 +1,115 @@
+// Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+// The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+// Example 1:
+
+// Input:
+// [
+//   ["5","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: true
+// Example 2:
+
+// Input:
+// [
+//   ["8","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being 
+//     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+// The given board contain only digits 1-9 and the character '.'.
+// The given board size is always 9x9.
+
+
+const isValidSudoku = (board) => {
+  for(let i = 0; i < board.length; i++){
+      let row = {};
+      let col = {};
+      for(let j = 0; j < board[i].length; j++){
+        if(board[i][j] !== '.') {
+          if(row[board[i][j]]) return false;
+          row[board[i][j]] = 1;
+        } 
+        if(board[j][i] !== '.') {
+          if(col[board[j][i]]) return false;
+          col[board[j][i]] = 1;
+        }
+      }
+    }
+
+  
+    for(let i = 0; i < board.length; i++){
+      let box = {};
+      let count = Math.floor(i / 3) * 3;
+      let count2 = (i % 3) * 3;
+      for(let j = 0; j < 3; j++){
+        for(let k = 0; k < 3; k++){
+          let current = board[count + j][count2 + k];
+          if(current !== '.'){
+            if(box[current]) return false;
+            box[current] = 1;
+          }
+        }
+      }
+    }
+ 
+    return true;
+};
+
+
+
+const board1 = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+  ];
+
+const result1 = isValidSudoku(board1);
+console.log(result1);
+
+
+const board2 = [
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+];
+
+const result2 = isValidSudoku(board2);
+console.log(result2);
--- Trees & Graphs/BSTSequences.js
+++ Trees & Graphs/BSTSequences.js
@@ -1,3 +1,5 @@
+const { BinarySearchTree } = require('../Data Structures/BinarySearchTree');
+
 // A binary search tree was created by traversing through an array from left to right and inserting each element. 
 // Given a binary search tree with distinct elements, print all possible arrays that could have led to this tree.
 //EXAMPLE
@@ -6,4 +8,94 @@
   // 1   3
 // Output: {2, 1, 3}, {2, 3, 1}
 
+/**
+ * The basic premise here is to get all permutations of each subtree's left and
+ * right children. Then create a new list of all the permutations from combining
+ * each permutation from the left subtree with each in the right subtree. The
+ * new set of permutations all need to be prefixed with the current nodes value
+ * as it must be added before its left or right children.
+ *
+ * The process of creating permutations requires combinging a list from the set
+ * of left child permutations and those of the right child. Each permutation
+ * needs to keep the items in their relative lists from their source lists.
+ *
+ * N = |tree|
+ * Time: O(N!) - this isn't really accurate and is definitely a high upper bound.
+ * Due to the fact that the permutations have some ordering required the true
+ * number isn't just the normal way to count permutations.
+ * Additional space: O(N!) - again like the time complexity this is a high upper
+ * bound
+ */
 
+
+const permutationsRecursive = (perms, list1, list2, prefix, i, j) => {
+  if(i === list1.length && j === list2.length){
+    perms.push(prefix.slice(0));
+  } else {
+    if(i < list1.length){
+      prefix.push(list1[i]);
+      permutationsRecursive(perms, list1, list2, prefix, i + 1, j);
+      prefix.pop();
+    }
+    if(j < list2.length){
+      prefix.push(list2[j]);
+      permutationsRecursive(perms, list1, list2, prefix, i, j + 1);
+      prefix.pop();
+    }
+  }
+  return perms;
+}
+
+const permutations = (left, right) => {
+  if(!left || !right){
+    return left || right;
+  } else {
+    let perms = [];
+    for(let i = 0; i < left.length; i++){
+      for(let j = 0; j < right.length; j++){
+        perms.push.apply(perms, permutationsRecursive([], left[i], right[j], [], 0, 0));
+      }
+    }
+    return perms;
+  }
+
+}
+
+const sequencesRecursive = (node) => {
+  if(!node){
+    return null;
+  } else {
+    let perms = permutations(sequencesRecursive(node.left), sequencesRecursive(node.right));
+    if(!perms){
+      perms = [[node.value]];
+    } else {
+      perms.forEach(p => p.unshift(node.value));
+    }
+    return perms;
+  }
+}
+
+
+const BSTSequences = (tree) => {
+  if(!tree){
+    return null;
+  }
+  return sequencesRecursive(tree);
+}
+
+
+
+let tree = new BinarySearchTree(50);
+tree.insert(20);
+tree.insert(60);
+tree.insert(10);
+tree.insert(25);
+tree.insert(70);
+tree.insert(5);
+tree.insert(15);
+tree.insert(65);
+tree.insert(80);
+console.log(tree);
+let result = BSTSequences(tree);
+console.log(result);
+console.log(result.length);
