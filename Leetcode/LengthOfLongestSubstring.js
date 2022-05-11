dd528cbde8d0edd326548af8b0508e06591fc357
Moyeong Lee <2000prcs@gmail.com>
2022-05-10 17:11:57 -0700

    add lengthOfLongestSubstring and removeDuplicates problems

---
+++ Leetcode/LengthOfLongestSubstring.js
@@ -0,0 +1,43 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+const lengthOfLongestSubstring = (string) => {
+    let i = 0;
+    let maxLength = 0;
+    const lookup = {};
+    
+    for (j = 0; j < string.length; j++) {
+        if (lookup[string[j]] !== undefined) {
+            i = Math.max(lookup[string[j]], i);
+        }
+        maxLength = Math.max(maxLength, j - i + 1);
+        lookup[string[j]] = j + 1;
+    }
+    return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb")) // 3 "abc"
+console.log(lengthOfLongestSubstring("bbbbb")) // 1 "b"
+console.log(lengthOfLongestSubstring("pwwkew")) // 3 "wke"
