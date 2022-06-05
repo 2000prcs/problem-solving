402448083ded29a63505eb770ae63f54ffe5629a
Moyeong Lee <2000prcs@gmail.com>
2022-06-04 18:41:49 -0700

    add helpful comments

--- Leetcode/kthLargestElement.js
+++ Leetcode/kthLargestElement.js
@@ -27,24 +27,38 @@ const findKthLargest = (nums, k) => {
     // return nums[k - 1];
 
  const quickSelect = (left, right) => {
-    const pivot = nums[right];
-    let pivotIndex = left;
-    
+    /**
+     * Use the last element as pivot for simplicity. Randomized pivot 
+     * is a better way to avoid worst case where the the largest or the smallest 
+     * element is always selected.
+     */
+    let pivot = nums[right];
+    let separatorIndex = left;
+    // Keep smaller numbers than pivot to the left of the separatorIndex and larger to the right
     for (let i = left; i < right; i++){
         if (nums[i] <= pivot) {
-            [nums[pivotIndex], nums[i]] = [nums[i], nums[pivotIndex]];
-            pivotIndex += 1;
+            [nums[separatorIndex], nums[i]] = [nums[i], nums[separatorIndex]];
+            separatorIndex += 1;
         }
 
     }
-    [nums[pivotIndex], nums[right]] = [nums[right], nums[pivotIndex]];
+    // Swap separatorIndex value to the position so that all numbers larger than the value 
+    // is on the right, and smaller on the left.
+    [nums[separatorIndex], nums[right]] = [nums[right], nums[separatorIndex]];
 
-    if (k < nums.length - pivotIndex) {
-        return quickSelect(pivotIndex + 1, right);
-    } else if (k > nums.length - pivotIndex) {
-        return quickSelect(left, pivotIndex - 1);
-    } else if (k === nums.length - pivotIndex) {
-        return nums[pivotIndex];
+     /**
+     * Use separatorIndex as the separator. If k is smaller than the length of the 
+     * right side of the array, the target is in the right side of the array. 
+     * If k is larger, the target is in the left side of the array. 
+     */
+    if (k < nums.length - separatorIndex) {
+        return quickSelect(separatorIndex + 1, right);
+    } else if (k > nums.length - separatorIndex) {
+        return quickSelect(left, separatorIndex - 1);
+    // separatorIndex is the index of the target if k equals to the length of the 
+    // right side of the array.
+    } else if (k === nums.length - separatorIndex) {
+        return nums[separatorIndex];
     }
  }
  return quickSelect(0, nums.length - 1);
